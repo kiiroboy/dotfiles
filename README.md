6c208b7f96d472c93edb65e46f54e012420366fa
Tuan Hoang <tath2701@gmail.com>
2023-04-27 13:41:49 +0700

    update ReadMe

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 - ln -s $PWD/kitty ~/.config/kitty
 - ln -s $PWD/neofetch ~/.config/neofetch
 - ln -s $PWD/nvim/init.vim ~/.config/nvim/init.vim
+```
+testing
+```
 
 
--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
 installOSX() {
-   BASEDIR=$(readlink -f $0 | xargs dirname)
-   for d in $BASEDIR/macOS/*; do
+    BASEDIR=$(readlink -f $0 | xargs dirname)
+    for d in $BASEDIR/macOS/*; do
         ln -sf $d $HOME/.config
     done
 }
+
 install() {
     case "$OSTYPE" in
       darwin*)  installOSX ;; 
