9ebe74ff33b2aaa0e87ffb10af970511e0608c22
Tuan Hoang <tath2701@gmail.com>
2023-04-27 13:36:14 +0700

    add installer

---
+++ install.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+installOSX() {
+   BASEDIR=$(readlink -f $0 | xargs dirname)
+   for d in $BASEDIR/macOS/*; do
+        ln -sf $d $HOME/.config
+    done
+}
+install() {
+    case "$OSTYPE" in
+      darwin*)  installOSX ;; 
+      linux*)   echo "LINUX" ;;
+      *)        echo "unknown: $OSTYPE" ;;
+    esac
+}
+install
