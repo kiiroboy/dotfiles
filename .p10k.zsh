3ab022beb06717981821ad2de3256d6eff9e0b3e
kiiroboy <tath2701@gmail.com>
2026-02-08 23:41:19 -0800

    update

--- .p10k.zsh
+++ .p10k.zsh
@@ -30,7 +30,7 @@
 
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    context                 # user@hostname
+  # context                 # user@hostname
     dir                     # current directory
     vcs                     # git status
     prompt_char             # prompt symbol
